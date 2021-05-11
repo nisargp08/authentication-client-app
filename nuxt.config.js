c1baf2b73108f0435de2ee995f637acaa03e1107
Nisarg Patel <nisargp42@gmail.com>
2021-05-11 16:10:52 -0400

    App title and favicon added

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'authentication-client-app',
+    title: `Nisarg's Authentication App`,
     htmlAttrs: {
       lang: 'en',
     },
