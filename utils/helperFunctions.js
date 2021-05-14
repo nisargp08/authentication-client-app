91a49e124cb6359cf18881ed804f1ce9c8761150
Nisarg Patel <nisargp42@gmail.com>
2021-05-14 02:44:43 -0400

    429 - Too many requests from one IP, error added

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -1,8 +1,13 @@
 export function setErrorMessage(err) {
   let errMessage = ''
   if (err.response) {
-    // client received an error response (5xx, 4xx)
-    errMessage = err.response.data.message
+    // Too many requests from one IP error
+    if (err.response.status === 429) {
+      errMessage = err.response.data
+    } else {
+      // Client recieved an operational error from our api
+      errMessage = err.response.data.message
+    }
   } else {
     // Unknown error occured
     errMessage = 'Network Error. Please try again later'
